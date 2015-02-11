3d0b929380a1f03bfe22ef8c8d62cf7977b0be41
FilipNilenius <webbkonton@gmail.com>
2015-02-11 16:40:02 +0100

    initial commit

---
+++ stationaryExample.m
@@ -0,0 +1,37 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Develop code using this file
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all
+close all
+
+addpath([pwd,'/classFiles'])
+addpath([pwd,'/misc'])
+warning('off','all')
+
+
+
+% % create object
+SVE = SVEclass;
+
+% % set properties
+SVE.nx                = 40;
+SVE.realizationNumber = 1;
+SVE.Lbox              = 2;
+SVE.aggFrac           = 0.3;
+
+% % input for 'generateSVE()'
+ballastRadii = [20 8 4 2]/2/10;   % Radius in [cm]. From http://www.sciencedirect.com/science/article/pii/S0168874X05001563
+gravelSieve  = [.25 .25 .35 .15]; % Distribution in fraction. sum(gravelSieve) should be 1.0
+aggFrac      = 0.30;              % Aggregate volume fraction
+Lbox         = 6;                 % size of SVE [cm]
+domainFactor = 1.5;               % ballast particles are distributed inside domainFactor*LBox
+
+
+% % apply methods
+SVE.setPath(); % uses current working directory
+% SVE.setPath('C:\optional\path'); % if you want files to be saved elsewhere
+SVE.generateSVE(SVE.aggFrac,ballastRadii,gravelSieve,SVE.Lbox,domainFactor);
+SVE.meshSVE();
+SVE.writeTopology();
+SVE.computeEffectiveDiffusivtyTensor();
---
+++ transientExample.m
@@ -0,0 +1,52 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Example of transient simulation.
+% Results are written to vtk-files.
+% You Paraview to visualize the results.
+%
+% The example takes approx. XX min. to run.
+%
+% http://www.paraview.org/
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clc
+clear all
+close all
+
+addpath([pwd,'/classFiles'])
+addpath([pwd,'/misc'])
+warning('off','all')
+
+
+
+% % create object
+SVE = SVEclass;
+
+% % set properties
+SVE.nx                = 60;
+SVE.realizationNumber = 1;
+SVE.Lbox              = 2;
+SVE.aggFrac           = 0.4;
+
+
+% % input for 'generateSVE()'
+ballastRadii = [20 8 4 2]/2/10;   % Radius in [cm]. From http://www.sciencedirect.com/science/article/pii/S0168874X05001563
+gravelSieve  = [.25 .25 .35 .15]; % Distribution in fraction. sum(gravelSieve) should be 1.0
+aggFrac      = 0.30;              % Aggregate volume fraction
+Lbox         = 6;                 % size of SVE [cm]
+domainFactor = 1.5;               % ballast particles are distributed inside domainFactor*LBox
+
+
+% % input for 'LinTransSolver()' method
+time.steps = 10;
+time.stepsize = 0.1;
+initialCondition = 1;
+
+
+% % apply methods
+SVE.setPath(); % uses current working directory
+%SVE.setPath('C:\optional\path'); % if you want files to be saved elsewhere
+SVE.generateSVE(SVE.aggFrac,ballastRadii,gravelSieve,SVE.Lbox,domainFactor);
+SVE.meshSVE();
+SVE.writeTopology();
+SVE.LinTransSolver(initialCondition,time);
+SVE.TransPostProcessor();
