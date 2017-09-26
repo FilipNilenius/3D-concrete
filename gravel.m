3c0b42b3a90ef1eb1caa48cb1c8f4e3ddc8738b4
Filip Nilenius <webbkonton@gmail.com>
2017-09-26 12:52:48 +0200

    initial commit

---
+++ gravel.m
@@ -0,0 +1,17 @@
+classdef gravel
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        radius
+        coordinate = struct('current',[],'atCollision',[]);
+        velocity = struct('current',[],'atCollision',[]);
+        timeAtCollision
+        storedCoordinates
+    end
+    
+    methods
+    end
+    
+end
+
