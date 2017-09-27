acc7b31ff416f1bfd6ca82e5a1e274dd86cecb88
Filip Nilenius <f.nilenius@gmail.com>
2017-09-27 13:11:02 +0200

    clean up

--- simpleCube.m
+++ simpleCube.m
@@ -7,8 +7,8 @@ gravelSet(1) = gravel;
 gravelSet(2) = gravel;
 gravelSet(3) = gravel;
 
-gravelSet(1).radius = 0.04;
-gravelSet(1).coordinates = [0.26,0.5,0.5];
+gravelSet(1).radius = 0.013;
+gravelSet(1).coordinates = [0.28,0.5,0.5];
 gravelSet(2).radius = 0.04;
 gravelSet(2).coordinates = [0.5,0.25,0.5];
 gravelSet(3).radius = 0.04;
@@ -42,22 +42,23 @@ gravelSet(3).velocity = gravelSet(3).velocity/norm(gravelSet(2).velocity);
 % gravelSet(3).velocity = [0,-1,0];
 % gravelSet.speed = 0.1;
 
-numberOfEvents = 60;
+numberOfEvents = 500;
 numberOfgravelSets = 3;
 
+% create all gravelSet interaction combinations
+gravelCombinations = combnk(1:length(gravelSet),2);
 
 for j=1:length(gravelSet)
     allCoordinates.gravelSet(j).event(1,:) = gravelSet(j).coordinates;    
 end
 
 for i=1:numberOfEvents
-    [gravelSetCollisionTime collidingGravels velocities] = findMinimumgravelSetCollisionTime(gravelSet);
+    % compute time to next collision event, either gravel-gravel or
+    % gravel-wall
+    [gravelSetCollisionTime collidingGravels velocities] = findMinimumgravelSetCollisionTime(gravelSet,gravelCombinations);
     [timeToWallCollision collidingGravel velocity] = findMinimumgravelSetWallCollisionTimee(cube,gravelSet);
-    
-    
-    gravelSetCollisionTime;
-    timeToWallCollision;
     timeToNextEvent = min([gravelSetCollisionTime timeToWallCollision]);
+    
     % update gravelSet coordinates at time of collision
     for j=1:length(gravelSet)
         gravelSet(j).coordinates = gravelSet(j).coordinates + timeToNextEvent*gravelSet(j).velocity;
@@ -66,15 +67,17 @@ for i=1:numberOfEvents
         allCoordinates.gravelSet(j).event(i+1,:) = gravelSet(j).coordinates;
     end
     
-    % update velocity after collision
+    % update gravel velocity after collision
     if timeToWallCollision < gravelSetCollisionTime % if next event is wall collision
         gravelSet(collidingGravel).velocity = velocity.new;
-    else
+    else % if next event is gravel-gravel collision
         gravelSet(collidingGravels(1)).velocity = velocities.one.new;
         gravelSet(collidingGravels(2)).velocity = velocities.two.new;
     end
 end
 
+
+% plot events
 hold on
 for i=1:numberOfgravelSets
     plot3(allCoordinates.gravelSet(i).event(:,1),allCoordinates.gravelSet(i).event(:,2),allCoordinates.gravelSet(i).event(:,3))
@@ -82,21 +85,18 @@ for i=1:numberOfgravelSets
 end
 axis([0 1 0 1 0 1 0 1])
 axis square
-% grid on
-% hold off
+grid on
 
 
-function [collisionTime collidingGravels velocity] = findMinimumgravelSetCollisionTime(gravelSet)
-% create all gravelSet interaction combinations
-c = combnk(1:length(gravelSet),2);
-collisionTime = inf*ones(length(c),1);
-for i=1:length(c)
+function [collisionTime collidingGravels velocity] = findMinimumgravelSetCollisionTime(gravelSet,gravelCombinations)
+collisionTime = inf*ones(length(gravelCombinations),1);
+for i=1:length(gravelCombinations)
     % solving for time when gravelSets collide
     % http://geomalgorithms.com/a07-_distance.html#dist3D_Segment_to_Segment
     % https://en.wikipedia.org/wiki/Quadratic_equation
-    wnull = gravelSet(c(i,1)).coordinates - gravelSet(c(i,2)).coordinates;
-    p = 2*wnull*(gravelSet(c(i,1)).velocity - gravelSet(c(i,2)).velocity)'/norm(gravelSet(c(i,1)).velocity - gravelSet(c(i,2)).velocity)^2;
-    q = (wnull*wnull' - (gravelSet(c(i,1)).radius + gravelSet(c(i,2)).radius)^2)/norm(gravelSet(c(i,1)).velocity - gravelSet(c(i,2)).velocity)^2;
+    wnull = gravelSet(gravelCombinations(i,1)).coordinates - gravelSet(gravelCombinations(i,2)).coordinates;
+    p = 2*wnull*(gravelSet(gravelCombinations(i,1)).velocity - gravelSet(gravelCombinations(i,2)).velocity)'/norm(gravelSet(gravelCombinations(i,1)).velocity - gravelSet(gravelCombinations(i,2)).velocity)^2;
+    q = (wnull*wnull' - (gravelSet(gravelCombinations(i,1)).radius + gravelSet(gravelCombinations(i,2)).radius)^2)/norm(gravelSet(gravelCombinations(i,1)).velocity - gravelSet(gravelCombinations(i,2)).velocity)^2;
     
     t.plus = -p/2 + sqrt((p/2)^2 - q);
     t.minus = -p/2 - sqrt((p/2)^2 - q);
@@ -111,14 +111,14 @@ for i=1:length(c)
     end
 end
 [collisionTime index] = min(collisionTime);
-collidingGravels = [c(index,1) c(index,2)];
+collidingGravels = [gravelCombinations(index,1) gravelCombinations(index,2)];
 
 % compute updated speeds after collision
-Gnull = gravelSet(c(index,1)).velocity - gravelSet(c(index,2)).velocity;
-n = gravelSet(c(index,1)).coordinates - gravelSet(c(index,2)).coordinates;
+Gnull = gravelSet(gravelCombinations(index,1)).velocity - gravelSet(gravelCombinations(index,2)).velocity;
+n = gravelSet(gravelCombinations(index,1)).coordinates - gravelSet(gravelCombinations(index,2)).coordinates;
 n = n/norm(n);
-velocity.one.new = gravelSet(c(index,1)).velocity - (n*Gnull')*2*gravelSet(c(index,2)).mass/(gravelSet(c(index,1)).mass + gravelSet(c(index,2)).mass)*n;
-velocity.two.new = gravelSet(c(index,2)).velocity + (n*Gnull')*2*gravelSet(c(index,1)).mass/(gravelSet(c(index,1)).mass + gravelSet(c(index,2)).mass)*n;
+velocity.one.new = gravelSet(gravelCombinations(index,1)).velocity - (n*Gnull')*2*gravelSet(gravelCombinations(index,2)).mass/(gravelSet(gravelCombinations(index,1)).mass + gravelSet(gravelCombinations(index,2)).mass)*n;
+velocity.two.new = gravelSet(gravelCombinations(index,2)).velocity + (n*Gnull')*2*gravelSet(gravelCombinations(index,1)).mass/(gravelSet(gravelCombinations(index,1)).mass + gravelSet(gravelCombinations(index,2)).mass)*n;
 end
 
 
