949825f3ee8703436501b7a3eda37d5966fa626e
Filip Nilenius <f.nilenius@gmail.com>
2017-10-18 11:30:59 +0200

    to have the entire sphere contained within the cube

--- misc/findTimeToWallCollision.m
+++ misc/findTimeToWallCollision.m
@@ -4,7 +4,7 @@ function [timeToWallCollision] = findTimeToWallCollision(xnull,x,v1,v2,r,n,d)
 
 timeToWallCollision = inf*ones(length(d),1);
 for i=1:length(d)
-    timeToWallCollision(i) = (n(d(i,2),:)*(xnull(d(i,2),:) - x(d(i,1),:))')/(n(d(i,2),:)*(v1(d(i,1),:) - v2(d(i,2),:))');
+    timeToWallCollision(i) = (n(d(i,2),:)*(xnull(d(i,2),:) - x(d(i,1),:))' - r(d(i,1)))/(n(d(i,2),:)*(v1(d(i,1),:) - v2(d(i,2),:))');
 end
 end
 
