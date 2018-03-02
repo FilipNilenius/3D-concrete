e4975d10b88f8a4a2d42fd530c6377bd5fcbaf03
Filip Nilenius <f.nilenius@gmail.com>
2018-03-02 17:27:38 +0100

    seems to resolve the problem of spheres escaping the SVE

--- misc/findTimeToWallCollision.m
+++ misc/findTimeToWallCollision.m
@@ -4,7 +4,7 @@ function [t] = findTimeToWallCollision(xnull,x,v1,v2,r,n,d,t,combinationsRows)
 
 for j=1:length(combinationsRows)
     i = combinationsRows(j);
-    t(i) = (n(d(i,2),:)*(xnull(d(i,2),:) - x(d(i,1),:))' - r(d(i,1)))/(n(d(i,2),:)*(v1(d(i,1),:) - v2(d(i,2),:))');
+    t(i) = 0.5*(n(d(i,2),:)*(xnull(d(i,2),:) - x(d(i,1),:))' - r(d(i,1)))/(n(d(i,2),:)*(v1(d(i,1),:) - v2(d(i,2),:))');
 end
 
 % identify which gravelSet will collide with which wall
