3d4a0a101da3c5cb04dfa6d482a45d1211127201
Filip Nilenius <f.nilenius@gmail.com>
2018-05-31 10:59:01 +0200

    updates to the example script

--- stationaryExample.m
+++ stationaryExample.m
@@ -17,18 +17,23 @@ warning('off','all')
 SVE = SVEclass;
 
 % % set properties
-SVE.nx                = 60;
+SVE.nx                = 40;
 SVE.realizationNumber = 1;
-SVE.Lbox              = 3;
-SVE.aggFrac           = 0.25;
-maxTime = 0; % hours
+SVE.Lbox              = 2.0;
+SVE.aggFrac           = 0.3;
+maxTime = 1/60; % hours
 
 % % input for 'generateSVE()'
 M = dlmread('sieve-input.txt',',');
 ballastRadii = M(:,1)/2/10; % [20 8 4 2]/2/10;   % Radius in [cm]. From http://www.sciencedirect.com/science/article/pii/S0168874X05001563
 gravelSieve  = M(:,2)/sum(M(:,2)); % [.25 .25 .35 .15]; % Distribution in fraction. sum(gravelSieve) should be 1.0
-numberOfGravel = 5000;            % ballast particles are distributed inside domainFactor*LBox
+numberOfGravel = 500;            % ballast particles are distributed inside domainFactor*LBox
 
+
+% % input for 'LinTransSolver()' method
+time.steps       = 10;
+time.stepsize    = 0.1;
+initialCondition = eps;
 
 % % set SVE boundary type
 % % 1 = physical boundary where no aggregates cut the boundary surface
@@ -40,11 +45,32 @@ numberOfGravel = 5000;            % ballast particles are distributed inside dom
 % SVE.boundary.z.back  = 1;
 % SVE.boundary.z.front = 1;
 
+% set convective coefficient at the three 'plus' faces of the SVE 
+% SVE.convCoeff.x.back = 100;
+% SVE.convCoeff.y.back = inf;
+% SVE.convCoeff.z.back = inf;
+% SVE.convCoeff.y.front = inf;
+% SVE.convCoeff.z.back  = inf;
+% SVE.convCoeff.z.front = inf;
+
+% set ambient humidity around the SVE where convective coefficient ~=inf
+% SVE.ambHum.x.back  = 1;
+% SVE.ambHum.x.front = 0;
+% SVE.ambHum.y.back  = 0;
+% SVE.ambHum.y.front = 0;
+% SVE.ambHum.z.back  = 0;
+% SVE.ambHum.z.front = 0;
+
 
 % % apply methods
 SVE.setPath(); % uses current working directory
 % SVE.setPath('C:\optional\path'); % if you want files to be saved elsewhere
-SVE.generateSVE(ballastRadii,gravelSieve,numberOfGravel,maxTime);
+SVE.generateSVE(ballastRadii,gravelSieve,numberOfGravel);
+SVE.packSVE(maxTime);
+SVE.deleteOverlappingSpheres();
 SVE.meshSVE();
 SVE.writeTopology();
 SVE.computeEffectiveDiffusivtyTensor();
+% SVE.LinTransSolver(initialCondition,time);
+% SVE.TransPostProcessor();
+% [spatialDirection,slicedPlane] = SVE.plotTransientFront();
